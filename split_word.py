0021a494a78cd50e63c9dd2a7e2c3aa9275a01f4
liangchen <henry_liangc@163.com>
2017-12-11 17:24:31 +0800

    first commit

---
+++ split_word.py
@@ -0,0 +1,98 @@
+# coding=utf-8
+# author : liangchen
+# decription : class doc_splitter to split all words in one directory path,generate new files(with segmented words in)
+
+import os
+from multiprocessing import cpu_count
+import re
+from datetime import datetime
+
+from pathlib import Path
+import jieba
+
+class doc_splitter():
+	def __init__(self,srcdir,targetdir,stopword=None,userdict=None,parallel=True):
+		self.srcdir = srcdir
+		self.tagdir = targetdir
+
+		self.stopword = stopword  #dict
+		self.parallel = parallel
+		self.userdict = userdict
+
+		jieba.initialize()
+		if self.userdict:
+			jieba.load_userdict(userdict)
+
+		if self.parallel:
+			jieba.enable_parallel(cpu_count())
+
+	def utf8_one_doc(self,filepath):
+		if not filepath:
+			return
+
+		print u'time: %s ==> 转换文件%s为utf-8格式\n' % (datetime.now(),filepath)
+		filepath_utf8 = '/tmp' + os.path.sep + os.path.splitext(os.path.basename(filepath))[0] + '_utf8' + '.txt'
+
+		if os.path.exists(filepath_utf8):
+			print u'time: %s ==> 文件%s已经转换为%s\n' % (datetime.now(), filepath,filepath_utf8)
+			return filepath_utf8
+
+		with open(str(filepath),'r') as fr:
+			with open(filepath_utf8,'w') as fw:
+				line = fr.readline()
+				while line:
+					if line == '\r\n':
+						line = fr.readline()
+						continue
+					newline = line.decode('GB18030').encode('utf-8')
+					print newline
+					print >> fw, newline
+					line = fr.readline()
+
+		print u'time: %s ==> 文件%s已经转换为%s\n' % (datetime.now(), filepath, filepath_utf8)
+		return filepath_utf8
+
+	def split_one_doc(self,filepath):
+		if not filepath:
+			return
+
+		print u'time: %s ==> 对文件%s进行分词\n' % (datetime.now(), filepath)
+		filepath_segmented = self.tagdir + os.path.sep + os.path.splitext(os.path.basename(filepath))[0] + '_segmented' + '.txt'
+
+		with open(filepath,'r') as fr:
+			with open(filepath_segmented,'w') as fw:
+				line = fr.readline()
+				while line:
+					if line == '\r\n':
+						line = fr.readline()
+						continue
+					line_str = line.strip().decode('utf-8', 'ignore')  # 去除每行首尾可能出现的空格，并转为Unicode进行处理
+					line1 = re.sub("[\s+\.\!\/_,$%^*()?;；:-【】+\"\']+|[+－——！，;:：。？、~@#￥%……&*（）]+".decode("utf8"),
+								   "".decode("utf8"), line_str)
+
+					word_list = list(jieba.cut(line1,cut_all=False,HMM=True))  # 用结巴分词，对每行内容进行分词
+					if self.stopword:
+						out_str = [word+' ' for word in word_list if word not in self.stopword]
+					else:
+						out_str = word_list
+					fw.write(" ".join(out_str).strip().encode('utf-8') + '\n')  # 将分词好的结果写入到输出文件
+					line = fr.readline()
+
+		print u'time: %s ==> 文件%s分词结束，保存为%s\n' % (datetime.now(), filepath, filepath_segmented)
+		return filepath_segmented
+
+
+	def split_all(self):
+		p = Path(self.srcdir)
+		for txt in p.glob("**/*.txt"):
+			txt_utf8 = self.utf8_one_doc(str(txt))
+			self.split_one_doc(txt_utf8)
+
+		return True
+
+
+#class test:
+if __name__=='__main__':
+	ds = doc_splitter('/home/lc/ht_work/ML/old_txt','/home/lc/ht_work/ML/new_txt',None,'/home/lc/ht_work/ML/xw_parse/userdict.txt',True)
+	ds.split_all()
+
---
+++ word_vec.py
@@ -0,0 +1,133 @@
+# coding=utf-8
+# author : liangchen
+# decription :  class line_generator yield one line at a time
+# 			 :  class word_vector to train all words in one directory path,generate word vectors
+
+
+import gensim.models.word2vec as w2v
+import logging
+import numpy as np
+
+import os
+from multiprocessing import cpu_count
+from datetime import datetime
+
+import split_word
+
+
+class line_generator():
+	def __init__(self,srcdir):
+		self.srcdir = srcdir
+
+	def __iter__(self):
+		for fname in os.listdir(self.srcdir):
+			for line in open(os.path.join(self.srcdir, fname)):
+				yield line.split()
+
+
+class word_vector():
+	def __init__(self,linesrc,modelpath=None,retrain=True):
+		self.lines = linesrc
+
+		#self.sentences = w2v.LineSentence(self.lines)
+		#self.sentences = self.linesrc
+
+		if modelpath:
+			self.model = w2v.Word2Vec.load(modelpath)
+		else:
+			self.model = None
+		self.retrain = retrain
+
+	def train_model(self):
+		if not self.retrain:
+			return self.model
+
+		first_flag = 1
+
+		cpu_num = cpu_count()
+		print u'time: %s ==> 模型训练开始，使用%d核\n' % (datetime.now(), cpu_num)
+		vector_size = 50
+		skip_gram = 0
+		hierarch_sm = 1
+		negative_sample = 0
+		context_window = 5
+		word_min_count = 5
+		print u'time: %s ==> 模型参数： Vector size %d\tSkip-gram %d\tHierarchical softmax %d\tNeg Sampling %d\tWindows %d\tMin_count %d\n' % (datetime.now(), \
+																																		 vector_size,\
+																																		 skip_gram, \
+																																		 hierarch_sm, \
+																																		 negative_sample, \
+																																		 context_window, \
+																																		 word_min_count)
+		for fname in os.listdir(self.lines):
+			fullpath = os.path.join(self.lines, fname)
+			if first_flag:
+				self.model = w2v.Word2Vec(w2v.LineSentence(fullpath), size=50, sg=0, hs=1, negative=0, window=5,
+									  min_count=5, workers=cpu_num)
+				first_flag = 0
+			else:
+				old_corpus_count = self.model.corpus_count
+				self.model.build_vocab(w2v.LineSentence(fullpath),update=True)
+				new_example_count = self.model.corpus_count - old_corpus_count
+				self.model.train(w2v.LineSentence(fullpath),total_examples=new_example_count,epochs=self.model.iter)
+
+
+		print u'time: %s ==> 模型训练结束，使用%d核\n' % (datetime.now(), cpu_num)
+
+		return self.model
+
+	def update_model(self,newfile):
+		if not self.model:
+			return None
+
+		if not os.path.isfile(newfile):
+			return self.model
+
+		with open(newfile,'r') as f:
+			train_word_count = self.model.train(w2v.LineSentence(f))
+			print u'time: %s ==> 文件%s更新模型结束，更新%d个词\n' % (datetime.now(), newfile, train_word_count)
+
+		return self.model
+
+	def get_vecs(self):
+		if self.model:
+			return self.model.wv
+
+		return None
+
+	def get_word_vec(self,word):
+		if not self.model:
+			return None
+
+		return self.model[word]
+
+	def save_model(self,path):
+		if path:
+			self.model.save(path)
+
+		return True
+
+	def model_test(self):
+		print self.model.similarity('赵敏'.decode('utf-8'), '赵敏'.decode('utf-8'))
+		print self.model.similarity('赵敏'.decode('utf-8'), '周芷若'.decode('utf-8'))
+		print self.model.similarity('赵敏'.decode('utf-8'), '韦一笑'.decode('utf-8'))
+
+		for k in self.model.similar_by_word('张三丰'.decode('utf-8')):
+			print k[0], k[1]
+
+		print self.model.wv['张三丰'.decode('utf-8')]
+		return
+
+
+
+#class_test
+if __name__=='__main__':
+	ds = split_word.doc_splitter('/home/lc/ht_work/ML/old_txt', '/home/lc/ht_work/ML/new_txt', None,'/home/lc/ht_work/ML/xw_parse/userdict.txt', True)
+	ds.split_all()
+
+	#word_v = word_vector(line_generator('/home/lc/ht_work/ML/new_txt/'),None,True)
+	word_v = word_vector('/home/lc/ht_work/ML/new_txt/', None, True)
+	word_v.train_model()
+
+	word_v.model_test()
+
